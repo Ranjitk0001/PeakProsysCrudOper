da6ece7a768d7f50416bae4e62eed1425b12adbe
rk <ranjitkale0001@gmail.com>
2024-11-26 17:45:54 +0530

    1st configuaration
    It is basic version and mysql connection

--- InquiryCrud/src/main/java/com/peakprosys/demo/InquiryCrudApplication.java
+++ InquiryCrud/src/main/java/com/peakprosys/demo/InquiryCrudApplication.java
@@ -8,6 +8,7 @@ public class InquiryCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InquiryCrudApplication.class, args);
+		System.out.println("Welcome to peakProsys");
 	}
 
 }
