fdea00834b4dfd90e6a8ba84cb80a2884dcf05f0
rk <ranjitkale0001@gmail.com>
2024-11-26 17:40:02 +0530

    create project spring boot

---
+++ InquiryCrud/src/main/java/com/peakprosys/demo/InquiryCrudApplication.java
@@ -0,0 +1,13 @@
+package com.peakprosys.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryCrudApplication.class, args);
+	}
+
+}
---
+++ InquiryCrud/src/test/java/com/peakprosys/demo/InquiryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peakprosys.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
